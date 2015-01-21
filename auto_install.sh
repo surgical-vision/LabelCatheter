20670cd3b4999057a086f4f2f86a204bb940b8ac
Ping-Lin Chang <p.chang10@imperial.ac.uk>
2015-01-21 14:56:13 +0100

    downgrade required cmake version

--- auto_install.sh
+++ auto_install.sh
@@ -2,20 +2,23 @@
 set -e
 
 # Build Pangolin
+printf '\n\e[1;36m==== Building Pangolin ====\e[0;39m\n'
 git submodule update --init
 cd external/Pangolin
 cmake -DBUILD_PANGOLIN_VIDEO=OFF -DBUILD_EXAMPLES=OFF
 make -j8
 
 # Build the main
+printf '\n\e[1;36m==== Building LabelBspline ====\e[0;39m\n'
 cd ../../
-mkdir build
-mkdir bin
+mkdir -p build
+mkdir -p bin
 cd build
-cmake ../
+cmake ..
 make -j8
-cd ../
+cd ..
 
 #clean up
-rm -rf external/Pangolin
+printf '\n\e[1;36m==== Clean Up ====\e[0;39m\n'
+git submodule deinit -f external/Pangolin
 rm -rf build
