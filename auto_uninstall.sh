efd94d0a9e288c39214455d0bfefada941d82122
Ping-Lin Chang <p.chang10@imperial.ac.uk>
2015-02-10 02:02:22 +0000

    updated unintsall script

--- auto_uninstall.sh
+++ auto_uninstall.sh
@@ -1,8 +1,4 @@
 #!/bin/bash                                                                                                                                                                                               
-                                                                                 
-# Stop whenever errors happen                                                    
-set -e 
-
 rm /usr/local/lib/libpangolin.dylib
 rm /usr/local/bin/label_catheter
 rm /usr/local/bin/video_exporter
